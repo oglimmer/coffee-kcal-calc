aaf357780381297ba998cf81956ab90a506031ea
Oli Zimpasser <oglimmer@gmail.com>
2022-11-06 12:58:09 +0100

    prepare to serve for github pages

--- components.d.ts
+++ components.d.ts
@@ -7,28 +7,11 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
-    BButton: typeof import('bootstrap-vue-3')['BButton']
     BCard: typeof import('bootstrap-vue-3')['BCard']
-    BDropdownDivider: typeof import('bootstrap-vue-3')['BDropdownDivider']
-    BDropdownItem: typeof import('bootstrap-vue-3')['BDropdownItem']
     BForm: typeof import('bootstrap-vue-3')['BForm']
-    BFormCheckbox: typeof import('bootstrap-vue-3')['BFormCheckbox']
-    BFormCheckboxGroup: typeof import('bootstrap-vue-3')['BFormCheckboxGroup']
     BFormGroup: typeof import('bootstrap-vue-3')['BFormGroup']
     BFormInput: typeof import('bootstrap-vue-3')['BFormInput']
-    BFormSelect: typeof import('bootstrap-vue-3')['BFormSelect']
     BNav: typeof import('bootstrap-vue-3')['BNav']
-    BNavbar: typeof import('bootstrap-vue-3')['BNavbar']
-    BNavbarNav: typeof import('bootstrap-vue-3')['BNavbarNav']
     BNavItem: typeof import('bootstrap-vue-3')['BNavItem']
-    BNavItemDropdown: typeof import('bootstrap-vue-3')['BNavItemDropdown']
-    HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
-    IconCommunity: typeof import('./src/components/icons/IconCommunity.vue')['default']
-    IconDocumentation: typeof import('./src/components/icons/IconDocumentation.vue')['default']
-    IconEcosystem: typeof import('./src/components/icons/IconEcosystem.vue')['default']
-    IconSupport: typeof import('./src/components/icons/IconSupport.vue')['default']
-    IconTooling: typeof import('./src/components/icons/IconTooling.vue')['default']
-    TheWelcome: typeof import('./src/components/TheWelcome.vue')['default']
-    WelcomeItem: typeof import('./src/components/WelcomeItem.vue')['default']
   }
 }
